fa977b23a30a3fc22c7835e170aa4b25162fc959
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-04-04 07:37:19 -0400

    caso de prueba donde el cambio de retorno es de 2 bs

--- src/banco.js
+++ src/banco.js
@@ -1,6 +1,6 @@
 function retornarCambio(dinero){
     const unBoliviano="1bs";
-    return unBoliviano;
+    return dinero+"bs";
 }
 
 export default retornarCambio;
--- src/banco.spec.js
+++ src/banco.spec.js
@@ -3,4 +3,7 @@ describe ("tests para el cambio en bolivianos", ()=>{
     it("Deberia retornar que el cambio el cambio con la menor cantidad de monedas es de 1Bs", ()=>{
         expect(retornarCambio(1)).toEqual("1bs");
     });
+    it("Deberia retornar que el cambio con la menor cantidad de monedas es de 2Bs", ()=>{
+        expect(retornarCambio(2)).toEqual("2bs");
+    });
 });
